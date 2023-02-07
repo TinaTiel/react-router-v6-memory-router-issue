dab52e1d2e7e689e406235cf0ae8dba20634c581
TinaTiel <51816935+TinaTiel@users.noreply.github.com>
2023-02-07 08:54:01 +0100

    using async findBy* query

--- src/tests/navigation.test.js
+++ src/tests/navigation.test.js
@@ -2,14 +2,14 @@ import { screen, render } from "@testing-library/react"
 import { createMemoryRouter, RouterProvider } from "react-router";
 import routes from "../routes";
 
-test('when rendered, the redirect page is shown instead of the ignored page', () => {
+test('when rendered, the redirect page is shown instead of the ignored page', async () => {
 
     // When rendered at the default route (/)
     const router = createMemoryRouter(routes);
     render(<RouterProvider router={router} />)
 
     // We expect the root to always be rendered
-    const rootHeading = screen.getByRole('heading', {name: 'Root Page'});
+    const rootHeading = await screen.findByRole('heading', {name: 'Root Page'});
     // THE TEST FAILS HERE UNEXPECTEDLY, BUT WORKS FINE IN THE WILD. IT'S RENDERING THE EMPTY ROOT DIV.
     expect(rootHeading).toBeInTheDocument(); 
 
